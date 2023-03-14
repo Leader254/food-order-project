ddf9f2f78b25d291f3adcf7307e303233e632507
joseph-gitau <57210731+joseph-gitau@users.noreply.github.com>
2023-03-14 10:50:09 +0300

    fix payment bug

--- reg_exe.php
+++ reg_exe.php
@@ -4,6 +4,12 @@ if (!isset($_SESSION)) {
     session_start();
 }
 include("connection/connect.php");
+// $db = mysqli_connect("localhost", "shakingmachine_iorder", "icb*lGIIq;Q5", "shakingmachine_onlinefoodphp");
+if ($db) {
+    echo "db connected!";
+} else {
+    echo "db not connected!";
+}
 
 if (isset($_POST['submit'])) {
     $username = $_POST['username'];
@@ -30,17 +36,20 @@ if (isset($_POST['submit'])) {
 if (isset($_POST['pay-order'])) {
     $phone = mysqli_real_escape_string($db, $_POST['phone-number']);
     $item_total = 0;
+    $uid = $_SESSION["user_id"];
+    $order_ids = [];
     foreach ($_SESSION["cart_item"] as $item) {
-
         $item_total += ($item["price"] * $item["quantity"]);
+        $item_title = $item["title"];
+        $item_quantity = $item["quantity"];
+        $item_price = $item["price"];
 
-        $SQL = "insert into users_orders(u_id,title,quantity,price) values('" . $_SESSION["user_id"] . "','" . $item["title"] . "','" . $item["quantity"] . "','" . $item["price"] . "')";
-
-        mysqli_query($db, $SQL);
+        $sql = "INSERT INTO `users_orders`(`u_id`, `title`, `quantity`, `price`) VALUES ('$uid', '$item_title', '$item_quantity', '$item_price')";
+        mysqli_query($db, $sql);
         // get this order id
-        $order_id = mysqli_insert_id($db);
+        $tp_order_id = mysqli_insert_id($db);
+        array_push($order_ids, $tp_order_id);
     }
-
 
     // function_alert();
     // get user details
@@ -67,7 +76,10 @@ if (isset($_POST['pay-order'])) {
     // save to payment table
     $uid = $_SESSION["user_id"];
     echo $uid;
-    echo "order id: " . $order_id;
+    $order_id = implode(",", $order_ids);
+    echo $order_id;
+    // join order ids to string
+
     echo "total: " . $item_total;
     echo "phone: " . $phone;
     $sql3 = "INSERT INTO payments (uid, order_id, total) VALUES ('$uid', '$order_id', '$item_total')";
@@ -77,6 +89,7 @@ if (isset($_POST['pay-order'])) {
         // get request  to express-stk.php to initiate stk push
         // $url = "https://localhost/express-stk.php?phone=" . $phone . "&amount=" . $item_total . "&order_id=" . $order_id;https://iorder.shaking-machine.com/iorder/mpesaIntegration/express-stk.php?phone=0724772046&amount=1&order_id=10
         $url = "https://iorder.shaking-machine.com/iorder/mpesaIntegration/express-stk.php?phone=" . $phone . "&amount=" . $item_total . "&order_id=" . $order_id;
+        echo $url;
         unset($_SESSION["cart_item"]);
         unset($item["title"]);
         unset($item["quantity"]);
@@ -88,7 +101,6 @@ if (isset($_POST['pay-order'])) {
         echo "Error: " . $sql3 . "<br>" . mysqli_error($db);
     }
 }
-
 
 /* 
 https://iorder.shaking-machine.com/iorder/mpesaIntegration/express-stk.php?phone=%22%20.%20$phone%20.%20%22&amount=%22%20.%20$item_total%20.%20%22&order_id=%22%20.%20$order_id
